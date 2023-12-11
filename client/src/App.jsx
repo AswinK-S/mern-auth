2c1c0f76384f70fb7a6d62334a38d42d39e98af3
Aswin K S <aswinkalathur11@gmail.com>
2023-12-11 11:31:42 +0530

    added component Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,15 @@ import About from "./pages/About"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/Signin"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
+
+    { /* {header} */ }
+    <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
