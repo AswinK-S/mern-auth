6f9ef4b3b6bbfe980d09834aaf24b40425cc639b
Aswin K S <aswinkalathur11@gmail.com>
2023-12-18 19:02:43 +0530

    created user profile update route and api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,55 @@
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs'
+import User from "../models/user.model.js"
+
 export const test =(req,res)=>{
     res.json({
         message:'API is working'
     })
 }
+
+//update user
+export const updateUser = async (req, res, next) => {
+    
+    if (req.user.id !== req.params.id) {
+      return next(errorHandler(401, 'You can update only your account!'));
+    }
+
+    try {
+
+        try{
+        if (!req.body.username && !req.body.email) {
+            return next(errorHandler(400, 'Username or email is required for update.'));
+        } 
+         }catch (hashingError) {
+            return next(errorHandler(500, 'Error hashing password'));
+          }
+
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profilePicture: req.body.profilePicture,
+          },
+        },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        return next(errorHandler(404, 'User not found or update failed'));
+      }
+  
+
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,6 +27,7 @@ export default function Profile() {
     const fileName = new Date().getTime() + image.name
     const storageRef = ref(storage, fileName)
     const uploadTask = uploadBytesResumable(storageRef, image)
+
     uploadTask.on('state_changed',
       (snapshot) => {
         const progress = (snapshot.bytesTransferred /
@@ -45,24 +46,30 @@ export default function Profile() {
         )
       }
     )
+    
   }
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className="flex flex-col gap-4">
+
+        {/* image upload */}
         <input type="file" ref={fileRef} hidden
           accept="image/*" onChange={(e) => setImage(e.target.files[0])} />
-        <img src={ formData.profilePicture || currentUser.profilePicture} alt="profile"
+
+        <img src={formData.profilePicture || currentUser.profilePicture} alt="profile"
           className="w-24 h-24 self-center rounded-full object-cover cursor-pointer mt-2"
           onClick={() => fileRef.current.click()} />
+
         <p className="text-sm self-center">
           {
             imageError ? (<span className="text-red-700">Error uploading image(file size must be less than 2mb)</span>) :
-            imagePercent > 0 && imagePercent < 100 ? (<span className="text-orange-700">{`uploading: ${imagePercent}%`}</span>) :
-            imagePercent === 100 ? (<span className="text-green-700">Image uploaded successfully</span>) : ("")
+              imagePercent > 0 && imagePercent < 100 ? (<span className="text-orange-700">{`uploading: ${imagePercent}%`}</span>) :
+                imagePercent === 100 ? (<span className="text-green-700">Image uploaded successfully</span>) : ("")
           }
         </p>
+
         <input defaultValue={currentUser.username} type="text" id="usernmae" placeholder="Username"
           className="bg-slate-100 rounded-lg p-3" />
 
@@ -73,8 +80,9 @@ export default function Profile() {
           placeholder="Password" className="bg-slate-100 rounded-lg p-3" />
 
         <button className="bg-slate-700 text-white p-3
-      rounded-lg uppercase hover:opacity-95 disabled:opacity-80">update</button>
+        rounded-lg uppercase hover:opacity-95 disabled:opacity-80">update</button>
       </form>
+
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
